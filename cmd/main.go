d846dfeb4f7ee95b6dc131ee26c4a10eaad8f929
tilt <tilt@yandex-team.ru>
2022-03-15 16:49:49 +0300

    Add LRU usage

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,18 @@
-package cmd
+package main
+
+import (
+	"MAI-Backend/pkg/cache"
+	"fmt"
+)
+
+func main() {
+	lru := cache.NewLRU(100)
+
+	lru.Set("Jesse", "Pinkman")
+	lru.Set("Walter", "White")
+	lru.Set("Jesse", "James")
+	fmt.Println(lru.Get("Jesse"))
+
+	lru.Remove("Walter")
+	fmt.Println(lru.Get("Walter"))
+}
